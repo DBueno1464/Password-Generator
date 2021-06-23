51a5fe37d59f0082df60f8ad8619554c0dce3adf
DBueno1464 <DerrickBueno00@gmail.com>
2021-06-23 18:49:49 -0400

    attempting one criteria

--- script.js
+++ script.js
@@ -7,63 +7,46 @@ function generatePassword() {
   // Length of the password
   var userlength = Number(prompt("How long should the password be? Enter a number between 8-125."));
 
-  let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%&*()_+-=[]|,./?><";
+  // User criteria
+  var lower = prompt("Would you like top include LOWERCASE characters? (YES or NO)");
+  //var upper = prompt("Would you like top include UPPERCASE characters? (YES or NO)");
+  //var num = prompt("Would you like top include NUMERIC characters? (YES or NO)");
+  //var special = prompt("Would you like top include SPECIAL characters? (YES or NO)");
+
+  // determine which criteria to apply
+
+  lowerChar = "qwertyuiopasdfghjklzxcvbnm";
+  upperChar = "QWERTYUIOPASDFGHJKLZXCVBNM";
+  numChar = "0123456789";
+  specialChar = "!@#$%^&*(){}[]=<>/,.";
+
+  let usableChar = "";
+  
+  if (lower.toLowerCase == "yes"){
+      usableChar.concat(lowerChar);
+  }
+  console.log(usableChar);
+
+  /*if (upper.toLowerCase === "yes"){
+    usableChar = usableChar.concat(upperChar);
+  }
+
+  if (num.toLowerCase === "yes"){
+    usableChar = usableChar.concat(numChar);
+  }
+
+  if (special.toLowerCase === "yes"){
+    usableChar = usableChar.concat(specialChar);
+  }
+*/
+  var usableLength = usableChar.length;
   let password = "";
 
   for(var i = 0; i < userlength; i++){
-    password = password + values.charAt(Math.floor(Math.random()*Math.floor(values.length - 1)));
+    password = password + usableChar.charAt(Math.floor(Math.random()*Math.floor(usableLength - 1)));
   }
+
   document.querySelector("#password").textContent = password;
-
-  passwordText.value = password;
-
-
- 
-/*
-  // Ask what special characters will be used
-  var charactertypeLower = prompt("Would you like LOWERCASE characters? (YES or NO)");
-  var charactertypeUpper = prompt("Would you like UPPERCASE characters? (YES or NO)");
-  var charactertypeNum = prompt("Would you like NUMBER characters? (YES or NO)");
-  var charactertypeSpe = prompt("Would you like SPECIAL characters? (YES or NO)");
-
-  var setofCharacters = "";
-
-  
-  if (charactertypeLower.toLowerCase === "yes"){
-    var setofcharLower = "abcdefghijklmnopqrstuvwxyz";
-    setofCharacters.concat(setofcharLower);
-  } else {
-    setofcharLower = "";
-  }
-
-  if (charactertypeUpper.toLowerCase === "yes"){
-    var setofcharUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  } else {
-    setofcharUpper = "";
-  }
-
-  if (charactertypeNum.toLowerCase === "yes"){
-    setofcharNum = "0123456789";
-  } else {
-    setofcharNum = "";
-  }
-
-  if (charactertypeSpe.toLowerCase === "yes"){
-    setofcharSpe = "!@#$%&*()_+-=[]|,./?><";
-  } else {
-    setofcharSpe = "";
-  }
-
-  //var setofCharacters = setofcharLower//.concat(setofcharUpper, setofcharNum, setofcharSpe);
-  var password = "";
-
-  // for loop to iterate over our array equal to the length of our password
-  for(var i = 0; i <= userlength; i++){
-    var randomIndex = Math.floor(Math.random()*setofCharacters.length);
-    password = password.concat(setofCharacters[randomChar]);
-  }
-  password = password.join('');
-  */
 }
 /*
 // Write password to the #password input
